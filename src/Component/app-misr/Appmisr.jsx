a2418c5a587eef9dea4fc2cb6ec58a66426580f5
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2025-07-15 17:48:00 -0700

    feat: finish part one updates

--- src/Component/app-misr/Appmisr.jsx
+++ src/Component/app-misr/Appmisr.jsx
@@ -20,12 +20,12 @@ const Appmisr = () => {
                     <p className='fs-4'>يعتبر موقع عقار يا مصر الموقع العقاري المجاني الأول في مصر حيث يمكنك البحث من خلال ماب الموقع عن عقارات للبيع او عقارات للإيجار بمساحات مختلفة وبالصور والأسعار من خلال متابعة اعلانات الموقع.</p>
                     <div className='text-center  pb-5 ffff  pt-5'>
                         <div className='fff'>
-                            <a className="in me-3  " target="_blank" href="https://play.google.com/store/apps/details?id=aqar.ya.masr"><img src="https://aqaryamasr.com/assets/front/imgs/in.png" alt="تطبيق ايفون عقار يا مصر" /></a>
+                            <a className="in " target="_blank" href="https://play.google.com/store/apps/details?id=aqar.ya.masr"><img src="https://aqaryamasr.com/assets/front/imgs/in.png" alt="تطبيق ايفون عقار يا مصر" /></a>
                         </div>
                     </div>
                 </Col>
                 <Col className='parent-image-app' data-aos="fade-up">
-                    <img src="/images/Ya-Masr-app.webp" className='omage-app' alt="" />
+                    <img src="/images/Ya-Masr-app.webp" className='image-app ' alt="" />
                 </Col>
 
             </Row>
