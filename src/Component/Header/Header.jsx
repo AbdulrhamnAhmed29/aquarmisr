65ecaf366183dbfbc355bd4382d010cce89ce499
AbdulrhamnAhmed29 <abdulrhmanaahmed@gmail.com>
2025-07-18 18:39:42 -0700

    شرح التعديل اللي عملته هنا

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -11,21 +11,14 @@ function IndividualIntervalsExample() {
     });
   }, []);
   return (
-    <div className="video-background" data-aos="fade-up mt-5">
-      <video autoPlay muted loop playsInline className="video">
-        <source src="/images/hero.mp4" type="video/mp4" />
-        المتصفح لا يدعم تشغيل الفيديو
-      </video>
-      <div className="overlay"></div>
-
-      <div className="content" data-aos="fade-left">
+    <header className="bg">
+      <div className="content p-5" data-aos="fade-left">
         <h2 className="fs-1">مرحبا بك في عقار يامصر </h2>
         <h2 className="fs-1">اكتشف أفضل الفرص العقارية في مصر</h2>
         <p>شقق – فيلات – أراضي – وحدات تجارية</p>
         <p>كل ما تحتاجه في مكان واحد... بكل ثقة وشفافية</p>
       </div>
-    </div>
-
+    </header>
 
   );
 }
